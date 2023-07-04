a0d0b2aa5049f3708ec8b09d9e967b0e785094ee
yang20002020 <2628636987@qq.com>
2023-07-04 09:57:48 +0800

    入门案例

---
+++ springboot_001_quickstart/src/test/java/com/fuyu/springboot_001_quickstart/Springboot001QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuyu.springboot_001_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot001QuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
