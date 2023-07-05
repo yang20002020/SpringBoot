52fe28a491f2f46beaf03fc7dfe145420a8dd6f5
yang20002020 <2628636987@qq.com>
2023-07-05 16:56:24 +0800

    多环境开发兼容问题（maven与boot）

---
+++ springboot_008_maven_and_boot_profile/src/main/java/com/itheima/Springboot008MavenAndBootProfileApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot008MavenAndBootProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot008MavenAndBootProfileApplication.class, args);
+    }
+
+}
