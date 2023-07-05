f47b3071a3f5b0181185c477c79224e675096fc5
yang20002020 <2628636987@qq.com>
2023-07-05 20:41:36 +0800

    配置文件分类

---
+++ springboot_009_config_file/src/main/java/com/itheima/Springboot009ConfigFileApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot009ConfigFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot009ConfigFileApplication.class, args);
+    }
+
+}
---
+++ springboot_009_config_file/src/test/java/com/itheima/Springboot009ConfigFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot009ConfigFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
