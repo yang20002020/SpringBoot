5ee569c4f9d3d5f41d66f2b3845dc6f112ea9720
yang20002020 <2628636987@qq.com>
2023-07-06 17:01:26 +0800

    springboot 整合SSM

---
+++ springboot_014_ssm/src/main/java/com/itheima/Springboot014SsmApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot014SsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot014SsmApplication.class, args);
+	}
+
+}
