95df746ac72e77d522a0059fa325ae7986b9337d
yang20002020 <2628636987@qq.com>
2023-07-10 15:54:41 +0800

    基于阿里云创建springboot入门案例

---
+++ springboot_002_01_quickstartalibaba/src/main/java/com/fuyu/Springboot00201QuickstartalibabaApplication.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot00201QuickstartalibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot00201QuickstartalibabaApplication.class, args);
+    }
+
+}
