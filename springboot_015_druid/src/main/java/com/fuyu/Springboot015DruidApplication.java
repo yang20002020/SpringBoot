d1862fcedfc0f6e3f88ff29b1c44d03e691e6b58
yang20002020 <2628636987@qq.com>
2023-07-13 17:10:57 +0800

    springboot 整合druid

---
+++ springboot_015_druid/src/main/java/com/fuyu/Springboot015DruidApplication.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot015DruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot015DruidApplication.class, args);
+    }
+
+}
