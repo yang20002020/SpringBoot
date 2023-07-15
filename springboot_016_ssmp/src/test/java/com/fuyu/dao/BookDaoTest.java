cb6c1d8627367e2fbff877c936f814d3e4a32b1f
yang20002020 <2628636987@qq.com>
2023-07-15 09:55:17 +0800

    数据层 条件查询

--- springboot_016_ssmp/src/test/java/com/fuyu/dao/BookDaoTest.java
+++ springboot_016_ssmp/src/test/java/com/fuyu/dao/BookDaoTest.java
@@ -1,5 +1,7 @@
 package com.fuyu.dao;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fuyu.domain.Book;
@@ -54,5 +56,42 @@ public class BookDaoTest {
         System.out.println(page.getTotal());
         System.out.println(page.getPages());
         System.out.println(page.getRecords());
+    }
+    //条件查询
+    @Test
+    void testGetBy(){
+       // select *from tbl_book where name like %Spring%
+        QueryWrapper<Book> qw = new QueryWrapper<>();
+        qw.like("name","Spring");
+        bookDao.selectList(qw);
+    }
+    //方法二
+    @Test
+    void testGetBy2(){
+        LambdaQueryWrapper<Book> lqw=new LambdaQueryWrapper<Book>();
+        String name="Spring";
+        lqw.like(Book::getName,name);
+        bookDao.selectList(lqw);
+    }
+    //运行该测试用例会出现sql语句bug
+    @Test
+    void testGetBy3(){
+        LambdaQueryWrapper<Book> lqw=new LambdaQueryWrapper<Book>();
+        String name=null;
+        lqw.like(Book::getName,name);
+        bookDao.selectList(lqw);
+    }
+    //改进
+    @Test
+    void testGetBy4(){
+        LambdaQueryWrapper<Book> lqw=new LambdaQueryWrapper<Book>();
+        String name=null;
+        lqw.like(name !=null,Book::getName,name);//如果name不等于null,则执行该语句；否则不执行该语句
+        bookDao.selectList(lqw);
+    }
+    @Test
+    void testGetBy5(){
+        LambdaQueryWrapper<Book> lqw=new LambdaQueryWrapper<Book>();
+        bookDao.selectList(lqw);
     }
 }
