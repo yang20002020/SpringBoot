6f870a76fa5a63b552bef5d1ae7f1c0c942b5ee1
yang20002020 <2628636987@qq.com>
2023-07-26 11:00:13 +0800

    测试类中启动web环境

--- springboot_040_web_test/src/test/java/com/fuyu/config/MsgConfig.java
+++ springboot_040_web_test/src/test/java/com/fuyu/config/MsgConfig.java
@@ -2,6 +2,7 @@ package com.fuyu.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class MsgConfig {
 
---
+++ springboot_040_web_test/src/test/java/com/fuyu/webTest040.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class webTest040 {
+
+    @Test
+    void test() {
+
+    }
+
+}
