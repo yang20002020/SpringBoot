7fdb214dde119566c4ce2abd48066f173de55e09
yang20002020 <2628636987@qq.com>
2023-07-26 17:42:50 +0800

    匹配响应执行状态

--- springboot_041_web_test/src/main/java/com/fuyu/controller/BookController.java
+++ springboot_041_web_test/src/main/java/com/fuyu/controller/BookController.java
@@ -1,5 +1,4 @@
 package com.fuyu.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- springboot_041_web_test/src/test/java/com/fuyu/webTest041.java
+++ springboot_041_web_test/src/test/java/com/fuyu/webTest041.java
@@ -5,8 +5,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,6 +28,21 @@ class webTest041 {
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
           //执行请求
         ResultActions actions = mvc.perform(builder);
+    }
+
+    @Test
+    void testStatus(@Autowired MockMvc mvc) throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books1");
+        ResultActions actions = mvc.perform(builder);
+
+        //设定预期值 与真实值进行比较，成功测试通过，失败测试失败
+        //定义执行状态匹配器
+        StatusResultMatchers status = MockMvcResultMatchers.status();
+        //定义预期执行状态，预计本次调用是成功的，状态200
+        ResultMatcher ok = status.isOk();
+        //使用本次真实执行结果与预期结果进行对比
+        //添加预计值ok到本次调用过程中进行匹配
+        actions.andExpect(ok);
     }
 
 }
