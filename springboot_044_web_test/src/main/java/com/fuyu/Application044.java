58af888e83d48aaf3ba094f559c9a472a12f6891
yang20002020 <2628636987@qq.com>
2023-07-27 18:25:22 +0800

    整合匹配响应

---
+++ springboot_044_web_test/src/main/java/com/fuyu/Application044.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application044 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application044.class, args);
+    }
+
+}
