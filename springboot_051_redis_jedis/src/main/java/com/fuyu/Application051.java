ae17e9e740633a804d41f4502334ca530a47438b
yang20002020 <2628636987@qq.com>
2023-07-30 16:11:49 +0800

    jedis

---
+++ springboot_051_redis_jedis/src/main/java/com/fuyu/Application051.java
@@ -0,0 +1,13 @@
+package com.fuyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application051 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application051.class, args);
+    }
+
+}
